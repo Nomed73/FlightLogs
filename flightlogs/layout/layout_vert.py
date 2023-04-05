039456504e837403c7fbc69ceb69d990f8ba068f
Nomed73 <noe314@gmail.com>
2023-04-05 14:43:21 -0500

    removed repeating code from layout_cert.py

--- flightlogs/layout/layout_vert.py
+++ flightlogs/layout/layout_vert.py
@@ -25,14 +25,12 @@ bv_pad_b = 5
 buttons_vert = [
     [sg.Text('Options', size=(bv_w , bv_h), pad=((bv_pad_l,bv_pad_r),(bv_pad_t, bv_pad_b)), justification='left')],
     [sg.Button("Connect Drone", size=(bv_w , bv_h),  pad=((bv_pad_l,bv_pad_r),(bv_pad_t, bv_pad_b)), key = '-CONNECT-', disabled=False)], 
-    [sg.Button("Connect Drone", size=(bv_w , bv_h),  pad=((bv_pad_l,bv_pad_r),(bv_pad_t, bv_pad_b)), key = '-CONNECT-', disabled=False)], 
     [sg.Button('Save log', size=(bv_w , bv_h),  pad=((bv_pad_l,bv_pad_r),(bv_pad_t, bv_pad_b)), key = '-SAVE LOG-', disabled=True)],
     [sg.Button('Save as CSV', size=(bv_w , bv_h),  pad=((bv_pad_l,bv_pad_r),(bv_pad_t, bv_pad_b)), key = '-TO CSV-', disabled=True)],
     [sg.Button('Save as JSON',  size=(bv_w , bv_h),  pad=((bv_pad_l,bv_pad_r),(bv_pad_t, bv_pad_b)), key = '-TO JSON-', disabled=True)],
     [sg.Button('View Flight Review', size=(bv_w , bv_h),  pad=((bv_pad_l,bv_pad_r),(bv_pad_t, bv_pad_b)), key = '-FLIGHT REVIEW-', disabled=True)],
     [sg.Text('Flight Review will open in browser', visible=False, key = '-CHECK BROWSER-')],
-    [sg.Button('Exit', size=(bv_w , bv_h),  pad=((bv_pad_l,bv_pad_r),(bv_pad_t+15, bv_pad_b-5)), key = '-EXIT-')],    
-    [sg.Button('Exit', size=(bv_w , bv_h),  pad=((bv_pad_l,bv_pad_r),(bv_pad_t+15, bv_pad_b-5)), key = '-EXIT-')],    
+    [sg.Button('Exit', size=(bv_w , bv_h),  pad=((bv_pad_l,bv_pad_r),(bv_pad_t+15, bv_pad_b-5)), key = '-EXIT-')],     
 ]
 
 # list box to show the files, the ulogs
@@ -43,7 +41,6 @@ list_p_t = 5
 list_p_b = 5
 list_p_l = 5
 list_p_r = 5
-file_view = [  
 file_view = [  
     [sg.Text('ULOG', size=(30,1)), sg.Text('SIZE', size=(10,1))],
     [sg.Listbox(values=[], enable_events=True, size=(50, 15), pad=((list_p_l, list_p_r),(list_p_t, list_p_b)), key="-LOG LIST-")],
@@ -67,9 +64,7 @@ busy = [
 layout_vert = [
     [
         sg.Column(buttons_vert, size= (200, 300), vertical_alignment = 'top', key = '-SELECTIONS-'),
-        sg.Column(buttons_vert, size= (200, 300), vertical_alignment = 'top', key = '-SELECTIONS-'),
         sg.VSeparator(),
-        sg.Column(file_view, size=(300, 300), vertical_alignment = 'top'),
         sg.Column(file_view, size=(300, 300), vertical_alignment = 'top'),
     ],
     [ sg.HorizontalSeparator() ],
